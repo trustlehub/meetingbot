0a3f167d8ea9f2e9d4b34681a5e6645f7e89959c
lasan <limahaliyana@gmail.com>
2024-08-07 10:05:46 +0530

    updated requirements.txt

--- google_launcher.sh
+++ google_launcher.sh
@@ -1,2 +1,2 @@
-#!/bin/sh 
-PULSE_SINK=chrome_sink xvfb-run --listen-tcp --server-num=63 --auth-file=/tmp/xvfb.auth -s "-ac -screen 0 1920x1080x24" python -m src.meeting.googlebot https://meet.google.com/uvq-riyn-pmj 63 ws://localhost:7000 testid
+#!/bin/bash 
+PULSE_SINK=chrome_sink xvfb-run --listen-tcp --server-num=18 --auth-file=/tmp/xvfb.auth -s "-ac -screen 0 1920x1080x24" python -m src.meeting.googlebot https://meet.google.com/ysf-yeax-oof 18 ws://localhost:7000 testid
